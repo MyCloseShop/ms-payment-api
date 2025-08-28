599bf83b3e120f16629b27cdd757aadcacbaa30c
amara_s <amara_s@etna-alternance.net>
2025-08-28 16:21:11 +0200

    fix: Add Apache HttpClient 5 dependency and configure RestTemplate for PATCH support

--- src/main/java/com/etna/gpe/ms_payment_api/config/RestTemplateConfig.java
+++ src/main/java/com/etna/gpe/ms_payment_api/config/RestTemplateConfig.java
@@ -1,7 +1,10 @@
 package com.etna.gpe.ms_payment_api.config;
 
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 /**
@@ -12,6 +15,9 @@ public class RestTemplateConfig {
 
     @Bean
     public RestTemplate restTemplate() {
-        return new RestTemplate();
+        // Configure Apache HttpClient 5 to enable HTTP methods like PATCH
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory(httpClient);
+        return new RestTemplate(requestFactory);
     }
 }
